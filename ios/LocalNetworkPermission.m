f1baef5be93350f9d9c8fd43f44f8f9f83438bee
Léon Chabbey <leon.chabbey@gmail.com>
2022-01-27 14:55:32 +0100

    refactor: global

--- ios/LocalNetworkPermission.m
+++ ios/LocalNetworkPermission.m
@@ -1,94 +1,22 @@
 #import "LocalNetworkPermission.h"
-#include <dns_sd.h>
 
-@implementation LocalNetworkPermission {
-    BOOL isBlocked;
-    RCTPromiseResolveBlock resolve;
-    NSTimer* timer;
-    DNSServiceRef browseRef;
-}
+@implementation LocalNetworkPermission
 
-RCT_EXPORT_MODULE()
-+ (BOOL)requiresMainQueueSetup
-{
-  return YES;  // only do this if your module initialization relies on calling UIKit!
-}
-
-RCT_EXPORT_METHOD(check:(double) timeOut
+RCT_EXPORT_METHOD(check,
                   withResolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
 {
     if (@available(iOS 14, *)){
-        NSLog(@"check local network permission with timeout %f", timeOut);
-        if(self->timer!=nil){
-            [self->timer invalidate];
-            self->timer = nil;
-            reject(@"EDUP",@"please wait the check promise resolve",nil);
-            return;
-        }
-        if(self->browseRef!=nil){
-            DNSServiceRefDeallocate(self->browseRef);
-            self->browseRef = nil;
-        }
-        self->resolve = resolve;
         dispatch_async(dispatch_get_main_queue(), ^{
-            self->timer = [NSTimer scheduledTimerWithTimeInterval: timeOut
-                                                           target: self
-                                                         selector:@selector(onTimeOut:)
-                                                         userInfo: nil
-                                                          repeats:NO];
-            DNSServiceErrorType error = kDNSServiceErr_NoError;
-            error = DNSServiceBrowse(&(self->browseRef), 0, 0, @"_navicosettingsrestapi._tcp".UTF8String, NULL, browseCallback, (__bridge void *)(self));
-            if (error != kDNSServiceErr_NoError)
-            {
-                reject(@"EUNKOWN",@"can not start DNSServiceSetDispatchQueue",nil);
-                return;
-            }
-            error = DNSServiceSetDispatchQueue(self->browseRef, dispatch_get_main_queue());
-            if (error != kDNSServiceErr_NoError)
-            {
-                reject(@"EUNKOWN",@"can not start DNSServiceSetDispatchQueue",nil);
-                return;
-            }
+            NSLog(@"check local network permission with timeout");
+            LocalNetworkPrivacy* localNetworkPrivacy = [LocalNetworkPrivacy new];
+            [localNetworkPrivacy checkAccessState:^(BOOL granted) {
+                NSLog(@"Granted: %@", granted ? @"YES" : @"NO");
+                resolve(granted);
+            }];
         });
     }else{
-        resolve([NSNumber numberWithBool:TRUE]);
-        return;
+        resolve(true);
     }
 }
 
-#pragma MARK - check
--(void)resolvePromise:(BOOL) result {
-    if(self->resolve!=nil){
-        self->resolve([NSNumber numberWithBool:result]);
-        self->resolve = nil;
-    }
-}
--(void)onTimeOut:(NSTimer *)timer {
-    DNSServiceRefDeallocate(self->browseRef);
-    self->browseRef = nil;
-    self->timer = nil;
-    [self resolvePromise:YES];
-}
--(void)onResult {
-    if(self->timer){
-        [self->timer invalidate];
-        self->timer = nil;
-        DNSServiceRefDeallocate(self->browseRef);
-        self->browseRef = nil;
-        [self resolvePromise:NO];
-    }
-}
-
-static void browseCallback(DNSServiceRef sdRef, DNSServiceFlags flags, uint32_t interfaceIndex,
-                           DNSServiceErrorType errorCode, const char *serviceName,
-                           const char *regtype, const char *replyDomain, void *context) {
-    if (errorCode == kDNSServiceErr_PolicyDenied) {
-        NSLog(@"local network ungranted");
-        if(context!=nil){
-            LocalNetworkPermission* this = (__bridge LocalNetworkPermission *)(context);
-            [this onResult];
-        }
-    }
-}
-@end
